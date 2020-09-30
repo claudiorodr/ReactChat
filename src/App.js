b83d00fac04f1ce5451b65d51caf4e795473bff9
Claudio Duarte Rodrigues <cluadio.duarte.98@live.com.pt>
2020-09-30 11:52:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDtpWUrs3Ip3UkjzInrxJSMBCDHMkomLsI",
+  authDomain: "reactchat-691fb.firebaseapp.com",
+  databaseURL: "https://reactchat-691fb.firebaseio.com",
+  projectId: "reactchat-691fb",
+  storageBucket: "reactchat-691fb.appspot.com",
+  messagingSenderId: "597415200028",
+  appId: "1:597415200028:web:2f2055cc477046387ea6d9",
+  measurementId: "G-26QHE8DNLN"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const auth = firebase.auth();
+
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 
 function App() {
+
+  const [user] = useAuthState(auth)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+
     </div>
   );
+}
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return(
+    <button onClick={signInWithGoogle}>Sign In With Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser &&(
+    <button onClick={() => auth.SignOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+  const messagesRef = firestore.collection('messages')
+  const query = messagesRef.orderBy('createdAt').limit(25)
+  const [messages] = useCollectionData(query, { idField: 'id' })
+
+  return(
+    <>
+    <div>
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+    </div>
+    </>
+  )
+}
+
+function ChatMessage(props) {
+  const {text, uid} = props.message
+
+  return <p>{text}</p>
 }
 
 export default App;
